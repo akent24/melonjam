7f1e3c95f31d1f527621a1c8bb7c314b7ce3cbc0
akent24 <149242044+akent24@users.noreply.github.com>
2026-06-30 15:41:38 +0200

    Button sprites added, first level is being made

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -3,8 +3,3 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/control.tscn")
-
-
-
-func _on_h_slider_value_changed(value: float) -> void:
-	MusicManager.set_volume(value)
