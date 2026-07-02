ab54c02ebe4338e3d877233586b7b5f69160884a
akent24 <149242044+akent24@users.noreply.github.com>
2026-07-02 20:41:31 +0200

    attack animations added

--- scripts/main_hero.gd
+++ scripts/main_hero.gd
@@ -5,7 +5,7 @@ const SPEED = 70.0
 const JUMP_VELOCITY = -200.0
 var player_stats = { "player_hp": 7, 
 }
-
+var is_attacking := false
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * 0.5 * delta
@@ -17,19 +17,26 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 		if direction < 0:
 			$AnimatedSprite2D.flip_h = false
+			$Area2D.scale.x = 1
+			$Area2D.position.x = -8.4
 		else:
 			$AnimatedSprite2D.flip_h = true
+			$Area2D.scale.x = -1
+			$Area2D.position.x = 8.4
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if is_on_floor():
+	if is_on_floor() and is_attacking == false:
 		if velocity.length() == 0:
 			$AnimatedSprite2D.play("idle")
 		else:
 			$AnimatedSprite2D.play("walk")
 	move_and_slide()
+	attak()
 
 func attak() -> void:
 	if Input.is_action_just_pressed("attak"):
+		is_attacking = true
+		$AnimatedSprite2D.play("attak")
 		$Area2D/AnimatedSprite2D.visible = true
 		$Area2D/AnimatedSprite2D.play("attak")
 		$Area2D/CollisionShape2D.disabled = false
@@ -38,5 +45,6 @@ func get_damage() -> void:
 	pass
 
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+func _on_animated_sprite_2d_animation_finished() -> void:
+	$Area2D/AnimatedSprite2D.visible = false
+	is_attacking = false
