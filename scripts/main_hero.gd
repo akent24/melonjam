b1c93f18bc3b9467084be7e6a908489720cab607
akent24 <149242044+akent24@users.noreply.github.com>
2026-07-02 13:31:27 +0200

    player animations added

--- scripts/main_hero.gd
+++ scripts/main_hero.gd
@@ -9,17 +9,21 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
+		$AnimatedSprite2D.play("jamp")
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction < 0:
+			$AnimatedSprite2D.flip_h = false
+		else:
+			$AnimatedSprite2D.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if is_on_floor():
+		if velocity.length() == 0:
+			$AnimatedSprite2D.play("idle")
+		else:
+			$AnimatedSprite2D.play("walk")
 	move_and_slide()
